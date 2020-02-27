99636adb0e42efdabbe55c308b666224576b5a46
pzelasko <piotr.zelasko@spoken.com>
2020-02-27 14:53:48 -0500

    Path -> str

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=[s for s in Path('gsd/commands').glob('*') if not s.name.startswith('_')]
+    scripts=[str(s) for s in Path('gsd/commands').glob('*') if not s.name.startswith('_')]
 )
