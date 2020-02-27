b64d22d8874631b8925491c2134cee10e4fc6457
pzelasko <piotr.zelasko@spoken.com>
2020-02-27 14:51:02 -0500

    Register commands

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='gsd',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    scripts=[s for s in Path('gsd/commands').glob('*') if not s.name.startswith('_')]
 )
