6652bd842c3e57077ce4674e6f91ed3afa494342
pzelasko <piotr.zelasko@spoken.com>
2020-02-27 13:03:13 -0500

    Use tkagg for rendering

--- gsd/commands/accuracy_report.py
+++ gsd/commands/accuracy_report.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import click
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -10,6 +13,7 @@ def main(report):
     df = pd.read_csv(report, sep='\t')
     df = df[:-1]
     df[['train_objective', 'valid_objective']].plot()
+    plt.show()
 
 
 if __name__ == '__main__':
